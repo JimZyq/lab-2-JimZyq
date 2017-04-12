6f564b3de7c49812cd7953c5160a4fdd9f38df13
Yongqing Zhu <jimmyzb@icloud.com>
2017-04-12 09:57:11 -0700

    Lab2

--- lab02_ex0.asm
+++ lab02_ex0.asm
@@ -1,9 +1,19 @@
 ;=================================================
-; Name: 
-; Email:  
+; Name:Yongqing Zhu 
+; Email: jimmyzb@icloud.com 
 ; 
 ; Lab: lab 2
-; Lab section: 
-; TA: 
+; Lab section:1 
+; TA: Jason Golding
 ; 
 ;=================================================
+.ORIG x3000
+	LEA R0, MSG_TO_PRINT
+	PUTS
+
+	HALT
+
+	MSG_TO_PRINT .STRINGZ "Hello world!!!!!\n"
+
+.END
+
